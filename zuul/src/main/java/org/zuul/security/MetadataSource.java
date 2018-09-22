9a8061287d0dbdc92b6cb12d43f858d576635158
1216309226@qq.com <1216309226@qq.com>
2018-09-22 14:23:38 +0800

    init

---
+++ zuul/src/main/java/org/zuul/security/MetadataSource.java
@@ -0,0 +1,35 @@
+package org.zuul.security;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
+import org.zuul.repository.RoleRepository;
+
+public class MetadataSource implements FilterInvocationSecurityMetadataSource {
+
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	private HashMap<String, Collection<ConfigAttribute>> roles =null;
+	
+	@Override
+	public Collection<ConfigAttribute> getAttributes(Object object) throws IllegalArgumentException {
+		
+		
+		return null;
+	}
+
+	@Override
+	public Collection<ConfigAttribute> getAllConfigAttributes() {
+		return null;
+	}
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return true;
+	}
+
+}
